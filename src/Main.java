1e7fc8a49119b95f6560c1491cea54849a7f6770
banerjri <banerjri@mail.uc.edu>
2023-06-18 10:27:42 -0500

    numbers compared

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double Num1;
+        double Num2;
+        System.out.print("Enter your First number :");
+        System.out.println("\nEnter Your Second Number :");
+        if (in.hasNextInt())
+        {
+            Num1 = in.nextDouble();
+            in.nextLine();
+            Num2 = in.nextDouble();
+            in.nextLine();
+            if (Num1 == Num2)
+            {
+                System.out.print("The numbers are equal");
+            } else if (Num1 > Num2)
+            {
+                System.out.println("The Second number " + Num2 + "is less");
+            } else
+            {
+                System.out.println("The First number " + Num1 + "is less");
+            }
+
+        }
+        else
+        {
+            System.out.println("\nYou must enter valid number");
+            System.out.println("try again");
+            System.exit(0);
+        }
+
+
+
     }
 }
